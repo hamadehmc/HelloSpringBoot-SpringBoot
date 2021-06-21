0dda43168e84777c8199981caa4159dc0ec2a5dd
Mohamad Hamadeh <hamadehm@sheridancollege.ca>
2021-06-21 19:34:48 -0400

    created the empty project

---
+++ src/test/java/sheridan/hamadehm/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.hamadehm.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
