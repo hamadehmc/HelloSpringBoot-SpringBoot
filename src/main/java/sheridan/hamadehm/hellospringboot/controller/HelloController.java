ec50635f74b72f1a0930528581d6319168503625
Mohamad Hamadeh <hamadehm@sheridancollege.ca>
2021-06-21 20:07:22 -0400

    Added the controller

---
+++ src/main/java/sheridan/hamadehm/hellospringboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package sheridan.hamadehm.hellospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/Hello")
+    public String hello(){
+    return "Hello";
+}
+
+
+}
